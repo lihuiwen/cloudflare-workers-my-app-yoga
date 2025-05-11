5591d0b99e1e40d1ba71e335d4181b5717663514
lihuiwen <lihuiwen.mkv@gmail.com>
2025-05-11 14:16:10 +0800

    feat: test deploy

--- src/index.ts
+++ src/index.ts
@@ -14,32 +14,6 @@
 import { createSchema, createYoga } from "graphql-yoga";
 
 /**
- * Env 接口定义了 Cloudflare Workers 环境中可用的绑定资源
- * 实际使用时可以取消注释并配置
- */
-export interface Env {
-    // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-    // KV 存储命名空间示例 - 键值对永久存储
-    // MY_KV_NAMESPACE: KVNamespace;
-    //
-    // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-    // Durable Object 命名空间示例 - 有状态的对象存储
-    // MY_DURABLE_OBJECT: DurableObjectNamespace;
-    //
-    // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-    // R2 存储桶示例 - 类似 S3 的对象存储
-    // MY_BUCKET: R2Bucket;
-    //
-    // Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-    // 服务绑定示例 - 连接到其他 Workers 服务
-    // MY_SERVICE: Fetcher;
-    //
-    // Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-    // 队列绑定示例 - 用于异步任务处理
-    // MY_QUEUE: Queue;
-}
-
-/**
  * 创建 Yoga GraphQL 服务器实例
  * 这里使用泛型 <Env> 确保 Yoga 可以访问 Cloudflare Workers 的环境变量
  */
